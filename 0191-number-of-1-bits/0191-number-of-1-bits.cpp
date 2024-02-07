7a0c10972fa67cc338caee22905eb1d6a3b5787a
Veetrag Nahar <62690947+iamveetragnahar@users.noreply.github.com>
2024-02-06 22:00:58 -0600

    Time: 4 ms (32.34%), Space: 7.1 MB (12.92%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int total_num=0;
-        while(n!=0){
-        if(n&1==1){
-         ++total_num; }
-         n=n>>1;
-        }
-        return total_num;
+        int count=0;
+      while(n!=0){
+          if(n&1){
+          count++;}
+          n=n>>1;
+          }  
+        return count;
     }
 };
